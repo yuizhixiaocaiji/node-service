1fd08776885a6dbf06fc9d4aaa08f9fc5a8ea7eb
yuizhixiaocaiji <1390683299@qq.com>
2023-03-06 11:33:03 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,9 @@
+// 导入 express 模块
+const express = require("express");
+// 创建 express 的服务器实例
+const app = express();
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+  console.log("api server running at http://127.0.0.1:3007");
+});
